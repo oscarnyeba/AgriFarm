d76fff4f36d9f1ac45a62e09852d4801b957c278
= <oscarnyeba@gmail.com>
2024-09-14 09:25:16 +0300

    add external css and bootstrap

--- agriculture_project/agriculture_project/settings.py
+++ agriculture_project/agriculture_project/settings.py
@@ -86,6 +86,11 @@ DATABASES = {
 }
 }
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
